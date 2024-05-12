750352dcd21dfd827bf0313ad7255c7006b4747e
Your Name <kadry.nukr@ukr.net>
2024-05-12 04:16:27 +0300

    begin

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import 'modern-normalize'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
